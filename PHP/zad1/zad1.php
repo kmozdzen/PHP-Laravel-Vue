56b3b9ad20056cec2246af690c229b0147d41cfc
kmozdzen <kmozdzen2@Onet.pl>
2023-07-12 15:51:22 +0200

    initial commit

---
+++ PHP/zad1/zad1.php
@@ -0,0 +1,17 @@
+<?php
+    require __DIR__ . '/Pipeline.php';
+    
+    $pipeline = new Pipeline();
+    $result = $pipeline->make(
+        function($var) { 
+            return $var * 3; 
+        }, 
+        function($var) { 
+            return $var + 1; 
+        },
+        function($var) { 
+            return $var / 2; 
+        }
+    );
+
+    echo $result(3);
